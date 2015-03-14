e89b581ed6c5f6cbd9a9b4c0de8b123ad734fa36
SimMac <->
2015-03-14 19:06:01 +0100

    Revert "Version 2.0: Added Linux Support! As this is only has been tested on one machine so far, it is still experimental."
    This reverts commit 85a72315fedf824f7ef3d37f8e5fae3dd747ae4f.

--- install.sh
+++ install.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 clear
-echo -e "\n\nQuick adb/fastboot installer 2.0 .\n"
+echo -e "\n\nQuick adb/fastboot installer 1.2 .\n"
 
 #Gets location of the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#Check if linux or OS X and setting parameters
-if [ "$(uname -s)" == "Darwin" ]; then #OS X
-	INSTALLPATH="/usr/bin"
-	BINPATH="$DIR/osx"
-	MD5="md5 -q"
-else	#we assume it's (debian) linux
-	INSTALLPATH="/usr/local/bin"
-	BINPATH="$DIR/linux"
-	MD5="md5sum"
-fi
-
 #This checks the adb version of the provided bins and the installed adb.
-VERSION="$($BINPATH/./adb version | cut -d ' '  -f5)"
+VERSION="$($DIR/bins/./adb version | cut -d ' '  -f5)"
 { VERSIONINSTALLED="$(adb version | cut -d ' '  -f5)"; } &> /dev/null
 
 #This is checking if the installed adb version is up to date.
+#If it is, it will quit the script.
 if [ "$VERSION" == "$VERSIONINSTALLED" ]; then
 		
-		echo -e "Comparing checksums...\n"
 		#check md5 match
-		MD5BIN="$($MD5 $BINPATH/adb)"
-		MD5INSTALLED="$($MD5 $(which adb))"
-		if [ "$MD5BIN" != "$MD5INSTALLED" ]; then
-			echo -e "Although the version number of the adb binary didn't change, the file itself changed. "
+		MD5="$(md5 $DIR/bins/adb | cut -d " " -f4 )"
+		MD5INSTALLED="$(md5 $(which adb) | cut -d " " -f4)"
+		if [ "$MD5" != "$MD5INSTALLED" ]; then
+			echo -e "$MD5 $MD5INSTALLED Although the version number of the adb binary didn't change, the file itself changed. "
 			echo -e "Probably there was a minor patch in the binary. Do you want to update the adb binary?\n"
 			read -p "Please enter y(es) or n(o): " yn
 			case $yn in
@@ -36,8 +25,7 @@ if [ "$VERSION" == "$VERSIONINSTALLED" ]; then
 				[Nn]* ) echo -e "Quitting...\n\n"; exit 0;;
 			esac
 		else
-			echo -e "\nChecksums of installed and provided binary match.\n"
-			echo -e "Your adb binaries seem to be up to date. Please check https://github.com/simmac/minimal_adb_fastboot for updates or do a git pull!"
+			echo -e "Your adb binaries seem to be up to date. Please check github.com/simmac/minimal_adb_fastboot for updates!"
 			exit 0;
 		fi
 fi
@@ -48,9 +36,8 @@ echo -e "The installation process is now starting. Please enter your password if
 read -p "Press [ENTER] to install adb and fastboot."
 
 #INSTALLATION ROUTINE
-sudo cp $BINPATH/adb $INSTALLPATH/adb
-sudo cp $BINPATH/fastboot $INSTALLPATH/fastboot
-
+sudo cp $DIR/bins/adb /usr/bin/adb
+sudo cp $DIR/bins/fastboot /usr/bin/fastboot
 
 
 #check if installation was successful.
